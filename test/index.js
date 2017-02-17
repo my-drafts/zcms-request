777ec2fa1b82f4c4636f649332ff4fda1d4bc7d7
Sergej Kukharev <sergej.kucharev@gmail.com>
2017-02-17 15:32:13 +0200

    0.0.12

--- test/index.js
+++ test/index.js
@@ -1,25 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const zt = require('ztype');
-
-class X extends Array{
-	constructor(x){
-		super(1, 2, 3);
-		const THIS = this;
-		x.forEach(function(x){
-			THIS.push(x);
-		})
-	}
-}
-
-let x = new X([5, 6, 7]);
-console.log(x, zt.as(x), zt.like(x), x.constructor.name, zt.like(X), X.constructor.name, X.prototype.constructor.name);
-
-return ;
-
 const http = require('http');
 const ZRequest = require('../').ZRequest;
+const zt = require('ztype');
 
 http.createServer(function(request, response){
 	let req = new ZRequest(request);
