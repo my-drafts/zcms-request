2fb89b3f395868355ec411bc6c7f33671c43e927
Sergej Kukharev <sergej.kucharev@gmail.com>
2017-02-24 11:16:48 +0200

    0.0.17

--- test/index.js
+++ test/index.js
@@ -2,21 +2,14 @@
 
 'use strict';
 
-const a = {
-	b:1, a:2
-};
-
-console.log(Object.keys(a));
-
-for(let x in a){
-	console.log(x, a[x]);
-}
-
 const http = require('http');
 const ZRequest = require('../').ZRequest;
 const zt = require('ztype');
 
 http.createServer(function(request, response){
 	let req = new ZRequest(request);
-	console.log(req);
+	console.log(String(req));
+	req.init().then(function(){
+		console.log(String(req));
+	});
 }).listen(8080);
